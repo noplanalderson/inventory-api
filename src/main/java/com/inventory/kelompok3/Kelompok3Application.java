5ab902950439c51c638e62b4a6008fd372b10c07
Muhammad Ridwan Na'im <noplanalderson@gmail.com>
2024-04-28 17:02:23 +0700

    Spring Initialization

---
+++ src/main/java/com/inventory/kelompok3/Kelompok3Application.java
@@ -0,0 +1,13 @@
+package com.inventory.kelompok3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kelompok3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kelompok3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/inventory/kelompok3/Kelompok3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventory.kelompok3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kelompok3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

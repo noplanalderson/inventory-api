9b74b42cf25344f332dd6429430f901bfe5dfc3b
Muhammad Ridwan Na'im <noplanalderson@gmail.com>
2024-06-20 12:33:54 +0700

    Fix bug authentication API

--- src/main/java/com/inventory/kelompok3/config/SecurityConfig.java
+++ src/main/java/com/inventory/kelompok3/config/SecurityConfig.java
@@ -1,8 +1,8 @@
 package com.inventory.kelompok3.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
@@ -11,7 +11,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 /** 
  * Class SecurityConfig
  * Konfigurasi autentikasi API dengan Basic-Auth
@@ -22,6 +21,9 @@ import org.springframework.security.web.authentication.AuthenticationFailureHand
 @Configuration
 public class SecurityConfig  {
 
+    @Autowired
+    private CustomAuthEntryPoint customAuthEntryPoint;
+    
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
         UserDetails apiUser = User.withUsername("restapi_simdc")
@@ -35,18 +37,30 @@ public class SecurityConfig  {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(a ->  a.requestMatchers(
-                "/**",
-                "/v2/api-docs/**", 
-                "/v2/api-docs.yaml", 
-                "/swagger-resources/**"
-            ).hasRole("ADMIN").anyRequest().permitAll())
-            .httpBasic(Customizer.withDefaults());
+                "/v2/api-docs",
+                "/v3/api-docs/**",
+                "/swagger-resources",
+                "/swagger-resources/**",
+                "/configuration/ui",
+                "/configuration/security",
+                "/swagger-ui.html",
+                "/swagger-ui/**",
+                "/webjars/**",
+                "/swagger-ui/index.html",
+                "/swagger-ui/",
+                "/swagger-ui/index.html#/",
+                "/swagger-ui/index.html?configUrl=/v3/api-docs/swagger-config",
+                "/swagger-ui/index.html?configUrl=/v3/api-docs/swagger-config#/"
+            ).permitAll()
+            .requestMatchers(
+                "/users/**",
+                "/devices/**",
+                "/manufactures/**",
+                "/device-models/**",
+                "/groups/**"
+            ).hasRole("ADMIN").anyRequest().authenticated())
+            .httpBasic(httpBasic -> httpBasic.authenticationEntryPoint(customAuthEntryPoint));
         return http.build();
-    }
-
-    @Bean
-    public AuthenticationFailureHandler authenticationFailureHandler() {
-        return new CustomAuthFailure();
     }
 
     @Bean

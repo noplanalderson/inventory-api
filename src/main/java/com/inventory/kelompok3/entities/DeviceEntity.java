ce7411e1b8507cd914ae0cd2ac2efb6dd6ad71c8
Muhammad Ridwan Na'im <noplanalderson@gmail.com>
2024-06-25 00:10:11 +0700

    perf(entity): add unique constraint to serialNumber field

--- src/main/java/com/inventory/kelompok3/entities/DeviceEntity.java
+++ src/main/java/com/inventory/kelompok3/entities/DeviceEntity.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,7 +17,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "tb_devices")
+@Table(name = "tb_devices",uniqueConstraints={@UniqueConstraint(columnNames={"serialNumber"})})
 public class DeviceEntity {
 
     @Id
